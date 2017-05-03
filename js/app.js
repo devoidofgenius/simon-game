a7fc0db56febf89bee07700e196b0864246afc30
devoidofgenius <cellinger@gmail.com>
2017-05-03 15:37:17 -0400

    I'll fix deployment later. Lazy fix

--- js/app.js
+++ js/app.js
@@ -1,40 +1,44 @@
-let nodes = document.querySelectorAll('.btn');
-let startBtn = document.querySelector('#start');
-let newNumberBtn = document.querySelector('#newNumber');
-let displaySequence = document.querySelector('.sequence');
-let playerSequence = document.querySelector('.player');
-let score = document.querySelector('.count');
-let strict = document.querySelector('#strict');
-let reset = document.querySelector('.reset');
-let mode = document.querySelector('.mode');
-let flash = document.querySelector('.flash-msg');
+'use strict';
 
-let sequence = [];
-let player = [];
-let strictMode = false;
-let isPlaying = false;
-let playerTurn = false;
+var nodes = document.querySelectorAll('.btn');
+var startBtn = document.querySelector('#start');
+var newNumberBtn = document.querySelector('#newNumber');
+var displaySequence = document.querySelector('.sequence');
+var playerSequence = document.querySelector('.player');
+var score = document.querySelector('.count');
+var strict = document.querySelector('#strict');
+var reset = document.querySelector('.reset');
+var mode = document.querySelector('.mode');
+var flash = document.querySelector('.flash-msg');
+
+var sequence = [];
+var player = [];
+var strictMode = false;
+var isPlaying = false;
+var playerTurn = false;
 
 // Add eventListeners to each node
-nodes.forEach(node => node.addEventListener('click', addPlayerTurn));
+nodes.forEach(function (node) {
+  return node.addEventListener('click', addPlayerTurn);
+});
 
 function test() {
   if (playerTurn && player.length !== sequence.length) {
-    console.log("Click")
+    console.log("Click");
   }
 }
 // When node is clicked
 function addPlayerTurn(e) {
   if (playerTurn && player.length !== sequence.length) {
     player.push(Number(e.target.dataset.btn));
-    let audio = document.querySelector(`audio[data-sound="${player[player.length - 1]}"]`);
-    if(audio) audio.currentTime = 0;
-    if(audio) audio.play();
-    let button = document.querySelector(`.btn[data-btn="${player[player.length - 1]}"]`);
+    var audio = document.querySelector('audio[data-sound="' + player[player.length - 1] + '"]');
+    if (audio) audio.currentTime = 0;
+    if (audio) audio.play();
+    var button = document.querySelector('.btn[data-btn="' + player[player.length - 1] + '"]');
     button.classList.add('blink');
-    button.addEventListener('transitionend', function(e) {
+    button.addEventListener('transitionend', function (e) {
       button.classList.remove('blink');
-    })
+    });
 
     updateScore();
     checkPlayerMove();
@@ -47,30 +51,30 @@ function randomNum() {
 }
 
 // Add a random number the the seqence
-function addNumber(){
+function addNumber() {
   sequence.push(randomNum());
 }
 
 // Play
 function playGame() {
-  console.log("play")
-  setTimeout(function() {
+  console.log("play");
+  setTimeout(function () {
     addNumber();
     player = [];
     playSound();
     updateScore();
-  }, 1000)
+  }, 1000);
 }
 
 // Play
 function replay() {
-  console.log("replay")
-  setTimeout(function() {
+  console.log("replay");
+  setTimeout(function () {
     player = [];
     playSound();
     updateScore();
     flash.classList.remove('animate');
-  }, 2000)
+  }, 2000);
 }
 
 function tryAgain_flash() {
@@ -90,48 +94,41 @@ function winner_flash() {
 
 // Check if the players move matches the sequence
 function checkPlayerMove() {
-const winner = player.length === 20 && player[player.length - 1] === sequence[sequence.length - 1];
-
+  var winner = player.length === 20 && player[player.length - 1] === sequence[sequence.length - 1];
 
   if (player.length !== sequence.length) {
-    player.map((value, index) => {
+    player.map(function (value, index) {
 
       if (!strictMode && value !== sequence[index]) {
-        console.log(`!StrictMode && ${value} !== ${sequence[index]}`)
+        console.log('!StrictMode && ' + value + ' !== ' + sequence[index]);
         document.querySelector('audio[data-sound="tryagain"').play();
         tryAgain_flash();
         replay();
-      }
-      else if (strictMode && value !== sequence[index]) {
-        console.log(`StrictMode && ${value} !== ${sequence[index]}`)
+      } else if (strictMode && value !== sequence[index]) {
+        console.log('StrictMode && ' + value + ' !== ' + sequence[index]);
         document.querySelector('audio[data-sound="gameover"').play();
         gameOver_flash();
         resetGame();
-      }
-      else {
+      } else {
         return;
       }
-    })
-  }
-  else {
+    });
+  } else {
     if (winner) {
       resetGame();
-      setTimeout(function() {
+      setTimeout(function () {
         document.querySelector('audio[data-sound="winner"').play();
         winner_flash();
-      }, 1000)
-    }
-    else if (!strictMode && player[player.length - 1] !== sequence[sequence.length - 1]) {
+      }, 1000);
+    } else if (!strictMode && player[player.length - 1] !== sequence[sequence.length - 1]) {
       document.querySelector('audio[data-sound="tryagain"').play();
       tryAgain_flash();
       replay();
-    }
-    else if (strictMode && player[player.length - 1] !== sequence[sequence.length - 1]) {
+    } else if (strictMode && player[player.length - 1] !== sequence[sequence.length - 1]) {
       document.querySelector('audio[data-sound="gameover"').play();
       gameOver_flash();
       resetGame();
-    }
-    else {
+    } else {
       playGame();
     }
   }
@@ -139,8 +136,8 @@ const winner = player.length === 20 && player[player.length - 1] === sequence[se
 
 // Reset Game
 function resetGame() {
-  console.log("resetGame")
-  setTimeout(function() {
+  console.log("resetGame");
+  setTimeout(function () {
     sequence = [];
     player = [];
     strictMode = false;
@@ -149,34 +146,37 @@ function resetGame() {
     updateScore();
     checkBtnStatus();
     flash.classList.remove('animate');
-  }, 1000)
+  }, 1000);
 }
 
 // Plays sounds for sequence
 function playSound() {
-  for(let i = 0; i < sequence.length; i += 1 ) {
-    (function(index) {
-      setTimeout(function() {
-        let audio = document.querySelector(`audio[data-sound="${sequence[index]}"]`);
-        let button = document.querySelector(`.btn[data-btn="${sequence[index]}"]`);
-        audio.addEventListener('ended', function(e) {
-        })
+  var _loop = function _loop(i) {
+    (function (index) {
+      setTimeout(function () {
+        var audio = document.querySelector('audio[data-sound="' + sequence[index] + '"]');
+        var button = document.querySelector('.btn[data-btn="' + sequence[index] + '"]');
+        audio.addEventListener('ended', function (e) {});
         button.classList.add('blink');
-        button.addEventListener('transitionend', function(e) {
+        button.addEventListener('transitionend', function (e) {
           button.classList.remove('blink');
-        })
+        });
         audio.currentTime = 0;
         audio.play();
         if (index + 1 === sequence.length) playerTurn = true;
       }, i * 1000);
     })(i);
+  };
+
+  for (var i = 0; i < sequence.length; i += 1) {
+    _loop(i);
   }
 }
 
 // Update Display for Testing
 function updateScore() {
   // displaySequence.innerHTML = sequence;
-  sequence.length === 0 ? score.innerHTML= 0 : score.innerHTML = sequence.length - 1;
+  sequence.length === 0 ? score.innerHTML = 0 : score.innerHTML = sequence.length - 1;
   // playerSequence.innerHTML = player;
   // strictMode === false ? mode.innerHTML = "Strict Mode Off" : mode.innerHTML = "Strict Mode On";
 }
@@ -188,14 +188,14 @@ function checkBtnStatus() {
 }
 
 // Start Button
-startBtn.addEventListener('click', (e) => {
+startBtn.addEventListener('click', function (e) {
   !isPlaying ? isPlaying = true : isPlaying = false; // Toggle isPlaying
   checkBtnStatus();
   playGame(); // Play Game
 });
 
 // Strict mode toggle
-strict.addEventListener('click', (e) => {
+strict.addEventListener('click', function (e) {
   strictMode === false ? strictMode = true : strictMode = false; // Toggle strict on/off
   checkBtnStatus();
 });
